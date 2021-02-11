542ae3bf13ba82ae2cc7c4e82a0f7171d090e8f3
hihigupt <higupt@microsoft.com>
2021-02-12 04:09:57 +0530

    Add unit test to build (#6)

--- .build/install_softwares.sh
+++ .build/install_softwares.sh
@@ -3,3 +3,4 @@
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install -y gcc-multilib g++-multilib ninja-build gcovr libpcap-dev:i386 ethtool isc-dhcp-server libcmocka-dev:i386 unifdef gcc-arm-none-eabi dos2unix
+sudo apt install -y gcc-multilib g++-multilib ninja-build libcmocka-dev
