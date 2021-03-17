cc88322a4c737b61a5f94ea33739a27381f9d5ab
Dane Walton <dawalton@microsoft.com>
2021-03-17 07:30:53 -0700

    added note

--- source/include/azure_iot.h
+++ source/include/azure_iot.h
@@ -5,6 +5,11 @@
  * @file azure_iot.h
  *
  * @brief Azure IoT FreeRTOS middleware common APIs and structs
+ * 
+ * @note You MUST NOT use any symbols (macros, functions, structures, enums, etc.) 
+ * prefixed with an underscore ('_') directly in your application code. These symbols
+ * are part of Azure SDK's internal implementation; we do not document these symbols
+ * and they are subject to change in future versions of the SDK which would break your code.
  *
  */
 
