c768b071c818c07eab9849501c471308bec9f6d8
hihigupt <higupt@microsoft.com>
2021-03-24 16:54:12 -0700

    Add CoreMQTT port (#48)

---
+++ ports/coreMQTT/azure_iot_mqtt_port.h
@@ -0,0 +1,20 @@
+/* Copyright (c) Microsoft Corporation. All rights reserved. */
+/* SPDX-License-Identifier: MIT */
+
+/**
+ * @file azure_iot_mqtt_port.h
+ * @brief Defines Azure IoT MQTT port based on coreMQTT
+ * 
+ */
+
+#ifndef AZURE_IOT_MQTT_PORT_H
+#define AZURE_IOT_MQTT_PORT_H
+
+#include <stdio.h>
+
+#include "core_mqtt.h"
+
+/* Maps MQTTContext directly to AzureIoTMQTT */
+typedef struct MQTTContext AzureIoTMQTT_t;
+
+#endif // AZURE_IOT_MQTT_PORT_H
