c83f25face8e10cfc49838ae9b1584ce967357b6
Dane Walton <dane.walton@microsoft.com>
2021-04-02 07:58:42 -0700

    fix custom config

--- source/include/azure_iot.h
+++ source/include/azure_iot.h
@@ -17,9 +17,9 @@
 #define AZURE_IOT_H
 
 /* AZURE_IOT_CUSTOM_CONFIG allows building the azure iot library
- * without a custom config. If a custom config is provided, the
- * AZURE_IOT_CUSTOM_CONFIG macro should not be defined. */
-#ifdef AZURE_IOT_CUSTOM_CONFIG
+ * with a custom config. If a custom config is provided, the
+ * AZURE_IOT_CUSTOM_CONFIG macro should be defined. */
+#ifndef AZURE_IOT_CUSTOM_CONFIG
     /* Include custom config file before other headers. */
     #include "azure_iot_config.h"
 #endif
