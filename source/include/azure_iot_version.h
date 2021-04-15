6baf1021a2895259c3e29463206ecb6d21d5648c
Dane Walton <dane.walton@microsoft.com>
2021-04-15 01:41:06 -0700

    update to use stringify

--- source/include/azure_iot_version.h
+++ source/include/azure_iot_version.h
@@ -4,7 +4,7 @@
 /**
  * @file azure_iot_version.h
  *
- * @brief Provides version information.
+ * @brief Provides version information for the Azure IoT FreeRTOS Middleware.
  *
  * @note You MUST NOT use any symbols (macros, functions, structures, enums, etc.)
  * prefixed with an underscore ('_') directly in your application code. These symbols
@@ -15,20 +15,25 @@
 #ifndef AZURE_IOT_VERSION_H
 #define AZURE_IOT_VERSION_H
 
-/*/ The version in string format used for telemetry following the `semver.org` standard */
-/*/ (https://semver.org). */
-#define azureiotVERSION_STRING        "0.0.1-alpha"
+#define STRINGIFY(x) #x
 
-/*/ Major numeric identifier. */
+/* Major numeric identifier. */
 #define azureiotVERSION_MAJOR         0
 
-/*/ Minor numeric identifier. */
+/* Minor numeric identifier. */
 #define azureiotVERSION_MINOR         0
 
-/*/ Patch numeric identifier. */
+/* Patch numeric identifier. */
 #define azureiotVERSION_PATCH         1
 
-/*/ Optional pre-release identifier. SDK is in a pre-release state when present. */
+/* Optional pre-release identifier. SDK is in a pre-release state when present. */
 #define azureiotVERSION_PRERELEASE    "alpha"
+
+/* The version in string format used for telemetry following the `semver.org` standard */
+/* (https://semver.org). */
+#define azureiotVERSION_STRING         STRINGIFY(azureiotVERSION_MAJOR) "." \
+                                       STRINGIFY(azureiotVERSION_MINOR) "." \
+                                       STRINGIFY(azureiotVERSION_PATCH) "-" \
+                                       STRINGIFY(azureiotVERSION_PRERELEASE)
 
 #endif /* AZURE_IOT_VERSION_H */
