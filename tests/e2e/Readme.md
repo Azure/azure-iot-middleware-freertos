d20354cb7c138c36d80fed399981dbc565b3fa4d
Himanshu Gupta <higupt@microsoft.com>
2021-04-21 05:38:42 +0000

    [E2E] Add service side code

---
+++ tests/e2e/Readme.md
@@ -0,0 +1,59 @@
+# Azure IoT Middleware E2E Test Framework
+
+## Overview
+
+The files in this directory implement the device SDK portion of E2E testing of the Embedded SDK for Azure IoT Convenience layer. For example, there are tests to make sure that a C2D is going through an IoTHub for real (NOT UT), and are properly received on a simulated device running the device SDK.
+
+This is complicated by the fact that the Embedded SDK does *not* have a IoTHub service SDK.  So a naive approach of `CreateProcess(e2e_device_exe)` and having said process run the service and device SDK at the same time, checking internal state of each, will not work.
+
+The e2e tests for Azure IoT Convenience layer use a different approach, splitting the service and device portions into distinct units of execution.  Service application code, written in TypeScript using the IoTHub service SDK, will act as a test driver to an executable written in C simulating the device.
+
+## How to run the tests
+* Note: Currently these tests are only supported to run on Linux. Windows support will follow.
+
+* The service application test is written in TypeScript.  It uses [Mocha](https://mochajs.org/) as its test framework and the IoTHub service sdks, among other dependencies.
+
+* To run the e2e test framework:
+
+  * [Install software](../../.github/scripts/init_vm_network.sh)
+
+  * [Configure network](../../.github/scripts/init_vm_network.sh) **Note: This creates a veth1 interface for RTOS connectivity.**
+
+  * Send enviornment variables and execute [run.sh](./run.sh) script.
+  ``` sh
+  sudo bash -c "export IOTHUB_CONNECTION_STRING="<iothub_connection_string>";
+   export IOT_PROVISIONING_CONNECTION_STRING="<dps_connection_string>";
+   export IOT_PROVISIONING_SCOPE_ID="<dps_id_scope>";
+   tests/e2e/run.sh veth1"
+  ```
+  * Note: if an error pops up stating that it was unable to find .js files, the transpile may have failed, check your npm installs.
+
+## Service application and device interaction
+
+How do the Node service application and the C simulated device interact?
+
+* The [service](./service) directory contains code authored in TypeScript. This code simulates a service application using IOTHUB service SDK's interacting with a IOTHUB. The code:
+
+  * Creates a device on the IoThub, using IoTHub API's. This device is prefixed with the name `azure_services_port_e2e_` followed by a random string for uniqueness.
+
+  * Creates a process built from the [device](./device) directory, passing the device_id, module_id and sas_token as the only command line arguments.
+
+  * Waits a few seconds for the test device to connect.
+
+  * Tests the device, sending various commands and checking the state.
+
+  * On test completion, the test will:
+
+    * Send a special command telling the test to gracefully terminate.
+
+    * Delete the device on IoTHub.
+
+    * Attempt to terminate the test's process, assuming there is some failure where it cannot gracefully exit.
+
+* The [device](./device) directory contains a simulated device code that handle test commands.  It is written in C and is the component under test.
+
+  * On test initiation, the device uses the passed in parameter to connect to IoTHub service.
+
+  * The device will wait for C2D messages from IOTHub.
+
+  * Tests are initiated by command arriving as C2D message payload.
---
+++ tests/e2e/service/common/e2e_test_commands.ts
@@ -0,0 +1,181 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+'use strict'
+
+import { anHourFromNow } from 'azure-iot-common';
+import { SharedAccessSignature } from 'azure-iothub';
+
+//
+// isEmpty check if object is empty.
+//
+function checkIsEmpty(obj) {
+    for(let key in obj) {
+        if(obj.hasOwnProperty(key)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+//
+// CommandTestData stores the expected test data for a particular test run.  ALL tests - telemetry, properties, and commands -
+// are initiated by a command that this test framework invokes on the test executable.
+//
+class CommandTestData {
+    protected payload:Object
+    protected properties:Object
+
+    constructor(methodName:string) {
+        this.payload = {};
+        this.payload["method"] = methodName
+        this.properties = {}
+    }
+
+    Serialize(): string {
+        return JSON.stringify(this.payload)
+    }
+
+    GetMethod(): string {
+        return this.payload["method"];
+    }
+
+    GetProperties(): Object {
+        return this.properties
+    }
+
+    GetDataExpected(): string {
+        return JSON.stringify(this.payload)
+    }
+}
+
+//
+// CommandTestData_EchoCommands is echo
+//
+class CommandTestData_EchoCommands extends CommandTestData {
+    constructor(payload:string) {
+        super("echo")
+        this.payload["payload"] = payload
+        this.properties = {}
+    }
+}
+
+//
+// CommandTestData_ExitCommands is used for exiting
+//
+class CommandTestData_ExitCommands extends CommandTestData {
+    constructor() {
+        super("exit")
+    }
+}
+
+//
+// CommandTestData_SendTelemetryCommands is used for testing SendTelemetry
+//
+class CommandTestData_SendTelemetryCommands extends CommandTestData {
+    constructor(payload:string, properties) {
+        super("send_telemetry")
+        this.payload["payload"] = payload
+        if (!checkIsEmpty(properties)) {
+            this.payload["properties"] = properties
+        }
+        this.properties = properties
+    }
+
+    GetDataExpected(): string {
+        let message = ""
+
+        if (!checkIsEmpty(this.payload["payload"])) {
+            message += this.payload["payload"]
+        }
+
+        if (!checkIsEmpty(this.properties)) {
+            message += JSON.stringify(this.properties)
+        }
+
+        return message
+    }
+}
+
+//
+// CommandTestData_DeviceProvisioningCommands is used for testing Device Provisioning
+//
+class CommandTestData_DeviceProvisioningCommands extends CommandTestData {
+    protected result:Object;
+
+    constructor(serviceEndpoint:string, id_scope:string, enrollmentForSymKeyDevice:any) {
+        super("device_provisioning")
+        this.payload["id_scope"] = id_scope
+        this.payload["service_endpoint"] = serviceEndpoint;
+        this.payload["registration_id"] = enrollmentForSymKeyDevice.registrationId
+        this.payload["symmetric_key"] = enrollmentForSymKeyDevice.attestation.symmetricKey.primaryKey
+        this.result = {}
+        this.result["device"] = enrollmentForSymKeyDevice.deviceId
+        this.result["status"] = "OK"
+    }
+
+    GetDataExpected(): string {
+        return JSON.stringify(this.result)
+    }
+}
+
+//
+// CommandTestData_ReportedPropertiesCommands is used for testing Device twin reported properties
+//
+class CommandTestData_ReportedPropertiesCommands extends CommandTestData {
+    constructor(payload:string) {
+        super("reported_properties")
+        this.payload["payload"] = payload
+    }
+
+    GetDataExpected(): string {
+        return JSON.stringify(this.payload)
+    }
+}
+
+//
+// CommandTestData_VerifyDesiredPropertiesCommands is used for testing Device twin desired properties
+//
+class CommandTestData_VerifyDesiredPropertiesCommands extends CommandTestData {
+    protected result:Object;
+
+    constructor(desired_property_key:string, desired_property_value:string) {
+        super("verify_desired_properties")
+        this.payload["desired_property_key"] = desired_property_key;
+        this.payload["desired_property_value"] = desired_property_value;
+        this.result = {}
+        this.result[desired_property_key] = desired_property_value
+    }
+
+    GetDataExpected(): string {
+        return JSON.stringify(this.result)
+    }
+}
+
+//
+// CommandTestData_GetTwinPropertiesCommands is used for testing Device twin properties
+//
+class CommandTestData_GetTwinPropertiesCommands extends CommandTestData {
+    protected result:string;
+
+    constructor(twin:string) {
+        super("get_twin_properties")
+        this.result = twin
+    }
+
+    GetDataExpected(): string {
+        return this.result
+    }
+}
+
+export {
+    CommandTestData,
+    CommandTestData_EchoCommands,
+    CommandTestData_ExitCommands,
+    CommandTestData_SendTelemetryCommands,
+    CommandTestData_DeviceProvisioningCommands,
+    CommandTestData_ReportedPropertiesCommands,
+    CommandTestData_VerifyDesiredPropertiesCommands,
+    CommandTestData_GetTwinPropertiesCommands
+};
