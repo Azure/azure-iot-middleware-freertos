96bb51b79c36d9e43a59f7f03111c90f62e4da41
Dane Walton <dane.walton@microsoft.com>
2021-05-05 17:27:45 -0700

    install uncrustify

--- .github/scripts/style_check.sh
+++ .github/scripts/style_check.sh
@@ -7,6 +7,8 @@ set -o errexit # Exit if command failed.
 set -o nounset # Exit if variable not set.
 set -o pipefail # Exit if pipe failed.
 
+sudo apt-get install uncrustify
+
 uncrustify -c ../../uncrustify.cfg --check       \
 ../../source/*.c                                 \
 ../../source/include/*.h                         \
