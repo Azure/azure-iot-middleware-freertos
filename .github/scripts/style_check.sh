c5e81706a8960fcd3f5718babc8e1512b51a8efc
Dane Walton <dane.walton@microsoft.com>
2021-05-05 18:34:58 -0700

    read as C

--- .github/scripts/style_check.sh
+++ .github/scripts/style_check.sh
@@ -13,19 +13,19 @@ FIX=${1:-0}
 sudo apt-get install uncrustify
 
 if [ $FIX -ne 0 ]; then
-    uncrustify -c ./uncrustify.cfg --no-backup --replace \
-    ./source/*.c                                         \
-    ./source/include/*.h                                 \
-    ./source/interface/*.h                               \
-    ./ports/coreMQTT/*.c                                 \
-    ./ports/coreMQTT/*.h                                 \
-    ./tests/config_files/*.h                             \
-    ./tests/e2e/device/*.c                               \
-    ./tests/e2e/device/*.h                               \
-    ./tests/ut/*.h                                       \
+    uncrustify -c ./uncrustify.cfg -l C --no-backup --replace \
+    ./source/*.c                                              \
+    ./source/include/*.h                                      \
+    ./source/interface/*.h                                    \
+    ./ports/coreMQTT/*.c                                      \
+    ./ports/coreMQTT/*.h                                      \
+    ./tests/config_files/*.h                                  \
+    ./tests/e2e/device/*.c                                    \
+    ./tests/e2e/device/*.h                                    \
+    ./tests/ut/*.h                                            \
     ./tests/ut/*.c
 else
-    uncrustify -c ./uncrustify.cfg --check           \
+    uncrustify -c ./uncrustify.cfg -l C --check      \
     ./source/*.c                                     \
     ./source/include/*.h                             \
     ./source/interface/*.h                           \
