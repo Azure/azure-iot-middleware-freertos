0f63a7729095f2b3f19439e6cab6f379f8412696
Dane Walton <dane.walton@microsoft.com>
2021-05-17 14:15:49 -0700

    update script

--- .github/scripts/ci_tests.sh
+++ .github/scripts/ci_tests.sh
@@ -25,11 +25,17 @@ fi
 echo -e "Using FreeRTOS in libraries/FreeRTOS (`git name-rev --name-only HEAD`)"
 TEST_FREERTOS_SRC=`pwd`/libraries/FreeRTOS
 
+# Turn off exit on error briefly since we know the docs fail for FreeRTOS
 set +e
 echo -e "::group::Checking doxygen documentation matches sources"
-cmake -Bbuild -Dcheck_docs=ON -DCMAKE_C_COMPILER=clang -Dfreertos_directory=$TEST_FREERTOS_SRC -Dfreertos_port_directory=$TEST_FREERTOS_SRC/FreeRTOS/Source/portable/ThirdParty/GCC/Posix -Dconfig_directory=.github/config . || true
-cmake --build build 2> build.log
+cmake -Bbuild -Dcheck_docs=ON -DCMAKE_C_COMPILER=clang -Dfreertos_directory=$TEST_FREERTOS_SRC -Dfreertos_port_directory=$TEST_FREERTOS_SRC/FreeRTOS/Source/portable/ThirdParty/GCC/Posix -Dconfig_directory=.github/config .
+cmake --build build > /dev/null 2> build.log
 cat build.log | grep -A 3 -E 'azure.*\.h'
+
+# If grep "failed" then it means it didn't find any problems. Exit if found something.
+if [ $? -ne 0 ]; then
+  exit 1
+fi
 set -e
 
 echo -e "::group::Build using clang"
