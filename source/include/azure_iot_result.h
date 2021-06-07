0d00447bb19ec12fd1dafa976a81a948c553312b
Dane Walton <dane.walton@microsoft.com>
2021-06-07 16:37:01 -0700

    update get next properties

--- source/include/azure_iot_result.h
+++ source/include/azure_iot_result.h
@@ -28,20 +28,21 @@ typedef enum AzureIoTResult
     eAzureIoTSuccess = 0,
 
     /* === Core: Error results === */
-    eAzureIoTErrorFailed,               /**< There was a failure. */
-    eAzureIoTErrorInvalidArgument,      /**< Input argument does not comply with the expected range of values. */
-    eAzureIoTErrorPending,              /**< The status of the operation is pending. */
-    eAzureIoTErrorOutOfMemory,          /**< The system is out of memory. */
-    eAzureIoTErrorInitFailed,           /**< The initialization failed. */
-    eAzureIoTErrorSubackWaitTimeout,    /**< There was timeout while waiting for SUBACK. */
-    eAzureIoTErrorTopicNotSubscribed,   /**< Topic not subscribed. */
-    eAzureIoTErrorPublishFailed,        /**< Failed to publish. */
-    eAzureIoTErrorSubscribeFailed,      /**< Failed to subscribe. */
-    eAzureIoTErrorUnsubscribeFailed,    /**< Failed to unsubscribe. */
-    eAzureIoTErrorServerError,          /**< There was a server error in registration. */
-    eAzureIoTErrorItemNotFound,         /**< The item was not found. */
-    eAzureIoTErrorTopicNoMatch,         /**< The received message was not for the currently processed feature. */
-    eAzureIoTErrorTokenGenerationFailed /**< There was a failure. */
+    eAzureIoTErrorFailed,                /**< There was a failure. */
+    eAzureIoTErrorInvalidArgument,       /**< Input argument does not comply with the expected range of values. */
+    eAzureIoTErrorPending,               /**< The status of the operation is pending. */
+    eAzureIoTErrorOutOfMemory,           /**< The system is out of memory. */
+    eAzureIoTErrorInitFailed,            /**< The initialization failed. */
+    eAzureIoTErrorSubackWaitTimeout,     /**< There was timeout while waiting for SUBACK. */
+    eAzureIoTErrorTopicNotSubscribed,    /**< Topic not subscribed. */
+    eAzureIoTErrorPublishFailed,         /**< Failed to publish. */
+    eAzureIoTErrorSubscribeFailed,       /**< Failed to subscribe. */
+    eAzureIoTErrorUnsubscribeFailed,     /**< Failed to unsubscribe. */
+    eAzureIoTErrorServerError,           /**< There was a server error in registration. */
+    eAzureIoTErrorItemNotFound,          /**< The item was not found. */
+    eAzureIoTErrorTopicNoMatch,          /**< The received message was not for the currently processed feature. */
+    eAzureIoTErrorTokenGenerationFailed, /**< There was a failure. */
+    eAzureIoTErrorEndOfProperties        /**< End of properties when iterating with AzureIoTHubClientProperties_GetNextComponentProperty(). */
 } AzureIoTResult_t;
 
 #endif /* AZURE_IOT_RESULT_H */
