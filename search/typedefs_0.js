0083d7e4ddd1cd9256f140a9d74e8f211c0e0ca3
danewalton <danewalton@users.noreply.github.com>
2021-06-10 14:51:58 +0000

    Deploying to gh-pages from @ Azure/azure-iot-middleware-freertos@cc5352b0d77f00c4ed93002643334a42125e0473 🚀

--- search/typedefs_0.js
+++ search/typedefs_0.js
@@ -1,14 +1,15 @@
 var searchData=
 [
-  ['azureiotgetcurrenttimefunc_5ft_285',['AzureIoTGetCurrentTimeFunc_t',['../azure__iot_8h.html#ad72c3a80ab66f18762dfca4ff4bd918c',1,'azure_iot.h']]],
-  ['azureiotgethmacfunc_5ft_286',['AzureIoTGetHMACFunc_t',['../azure__iot_8h.html#a5824b14bbf563402fa8d15279fdb74cc',1,'azure_iot.h']]],
-  ['azureiothubclientcloudtodevicemessagecallback_5ft_287',['AzureIoTHubClientCloudToDeviceMessageCallback_t',['../azure__iot__hub__client_8h.html#a669b820c2f3a094fc51e3052586776c9',1,'azure_iot_hub_client.h']]],
-  ['azureiothubclientmethodcallback_5ft_288',['AzureIoTHubClientMethodCallback_t',['../azure__iot__hub__client_8h.html#abb69873b1952a73dda4362f33110aa91',1,'azure_iot_hub_client.h']]],
-  ['azureiothubclienttwincallback_5ft_289',['AzureIoTHubClientTwinCallback_t',['../azure__iot__hub__client_8h.html#a11b9847e2cbc1f914bcae385ded92cf3',1,'azure_iot_hub_client.h']]],
-  ['azureiotmqtteventcallback_5ft_290',['AzureIoTMQTTEventCallback_t',['../azure__iot__mqtt_8h.html#afe5870931c1aba4d3aff52aa8751a208',1,'azure_iot_mqtt.h']]],
-  ['azureiotmqttgetcurrenttimefunc_5ft_291',['AzureIoTMQTTGetCurrentTimeFunc_t',['../azure__iot__mqtt_8h.html#a4c3ba99ad053c362e284a26c89d6eeda',1,'azure_iot_mqtt.h']]],
-  ['azureiotmqtthandle_5ft_292',['AzureIoTMQTTHandle_t',['../azure__iot__mqtt_8h.html#ae10c68b0cfa37f4b6b9387ca84f6d0c3',1,'azure_iot_mqtt.h']]],
-  ['azureiottelemetryackcallback_5ft_293',['AzureIoTTelemetryAckCallback_t',['../azure__iot__hub__client_8h.html#af6adbb2afc1cfdb598bb2f9a6528907a',1,'azure_iot_hub_client.h']]],
-  ['azureiottransportrecv_5ft_294',['AzureIoTTransportRecv_t',['../azure__iot__transport__interface_8h.html#a1844f2f0ac918e0dbd8ebc3e9c473cef',1,'azure_iot_transport_interface.h']]],
-  ['azureiottransportsend_5ft_295',['AzureIoTTransportSend_t',['../azure__iot__transport__interface_8h.html#abbdced6c2ba7013184e22f1b31dd64a6',1,'azure_iot_transport_interface.h']]]
+  ['azureiotgetcurrenttimefunc_5ft_369',['AzureIoTGetCurrentTimeFunc_t',['../azure__iot_8h.html#ad72c3a80ab66f18762dfca4ff4bd918c',1,'azure_iot.h']]],
+  ['azureiotgethmacfunc_5ft_370',['AzureIoTGetHMACFunc_t',['../azure__iot_8h.html#a5824b14bbf563402fa8d15279fdb74cc',1,'azure_iot.h']]],
+  ['azureiothubclientcloudtodevicemessagecallback_5ft_371',['AzureIoTHubClientCloudToDeviceMessageCallback_t',['../azure__iot__hub__client_8h.html#a669b820c2f3a094fc51e3052586776c9',1,'azure_iot_hub_client.h']]],
+  ['azureiothubclientcommandcallback_5ft_372',['AzureIoTHubClientCommandCallback_t',['../azure__iot__hub__client_8h.html#a031f0aa52bf2547b72cd248f8f2afa69',1,'azure_iot_hub_client.h']]],
+  ['azureiothubclientcomponent_5ft_373',['AzureIoTHubClientComponent_t',['../azure__iot__hub__client_8h.html#a55ef8e20db98d7c1eb9c01732cdf9d0c',1,'azure_iot_hub_client.h']]],
+  ['azureiothubclientpropertiescallback_5ft_374',['AzureIoTHubClientPropertiesCallback_t',['../azure__iot__hub__client_8h.html#a6c774bbfd8711079885c9f6879f2cfeb',1,'azure_iot_hub_client.h']]],
+  ['azureiotmqtteventcallback_5ft_375',['AzureIoTMQTTEventCallback_t',['../azure__iot__mqtt_8h.html#afe5870931c1aba4d3aff52aa8751a208',1,'azure_iot_mqtt.h']]],
+  ['azureiotmqttgetcurrenttimefunc_5ft_376',['AzureIoTMQTTGetCurrentTimeFunc_t',['../azure__iot__mqtt_8h.html#a4c3ba99ad053c362e284a26c89d6eeda',1,'azure_iot_mqtt.h']]],
+  ['azureiotmqtthandle_5ft_377',['AzureIoTMQTTHandle_t',['../azure__iot__mqtt_8h.html#ae10c68b0cfa37f4b6b9387ca84f6d0c3',1,'azure_iot_mqtt.h']]],
+  ['azureiottelemetryackcallback_5ft_378',['AzureIoTTelemetryAckCallback_t',['../azure__iot__hub__client_8h.html#af6adbb2afc1cfdb598bb2f9a6528907a',1,'azure_iot_hub_client.h']]],
+  ['azureiottransportrecv_5ft_379',['AzureIoTTransportRecv_t',['../azure__iot__transport__interface_8h.html#a1844f2f0ac918e0dbd8ebc3e9c473cef',1,'azure_iot_transport_interface.h']]],
+  ['azureiottransportsend_5ft_380',['AzureIoTTransportSend_t',['../azure__iot__transport__interface_8h.html#abbdced6c2ba7013184e22f1b31dd64a6',1,'azure_iot_transport_interface.h']]]
 ];
