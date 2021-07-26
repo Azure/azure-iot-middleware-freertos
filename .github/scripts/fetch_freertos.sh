09d2bdd2d4e7b3de25f77aaa31c5edd38d562ad9
Himanshu Gupta <higupt@microsoft.com>
2021-07-26 19:55:51 +0000

    Fix freertos fetch script

--- .github/scripts/fetch_freertos.sh
+++ .github/scripts/fetch_freertos.sh
@@ -12,7 +12,7 @@ echo -e "::group::FreeRTOS Source"
 if [ ! -d "libraries/FreeRTOS" ]; then
     git clone https://github.com/FreeRTOS/FreeRTOS.git libraries/FreeRTOS
     pushd libraries/FreeRTOS
-    git checkout -b c8fa483b68c6c1149c2a7a8bc1e901b38860ec9b
+    git checkout c8fa483b68c6c1149c2a7a8bc1e901b38860ec9b
     git submodule sync
     git submodule update --init --recursive --depth=1
     popd
