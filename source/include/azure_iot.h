4811c4f9b95f01c3b0454e21c8e0219fa60d4fb7
Dane Walton <dane.walton@microsoft.com>
2021-08-13 17:37:25 -0700

    move hmac calculation to private header (#171)

--- source/include/azure_iot.h
+++ source/include/azure_iot.h
@@ -160,35 +160,6 @@ AzureIoTResult_t AzureIoT_MessagePropertiesFind( AzureIoTMessageProperties_t * p
                                                  const uint8_t ** ppucOutValue,
                                                  uint32_t * pulOutValueLength );
 
-/**
- * @brief As part of symmetric key authentication, HMAC256 a buffer of bytes and base64 encode the result.
- *
- * @note This is used within Azure IoT Hub and Device Provisioning APIs if a symmetric key is set.
- *
- * @param[in] xAzureIoTHMACFunction The #AzureIoTGetHMACFunc_t function to use for HMAC256 hashing.
- * @param[in] pucKey A pointer to the base64 encoded key.
- * @param[in] ulKeySize The length of the \p pucKey.
- * @param[in] pucMessage A pointer to the blob to be hashed.
- * @param[in] ulMessageSize The length of \p pucMessage.
- * @param[in] pucBuffer An intermediary buffer to put the base64 decoded key.
- * @param[in] ulBufferLength The length of \p pucBuffer.
- * @param[out] pucOutput The buffer into which the resulting HMAC256 hashed, base64 encoded message will
- * be placed.
- * @param[in] ulOutputSize Size of \p pucOutput.
- * @param[out] pulOutputLength The output length of \p pucOutput.
- * @return An #AzureIoTResult_t with the result of the operation.
- */
-AzureIoTResult_t AzureIoT_Base64HMACCalculate( AzureIoTGetHMACFunc_t xAzureIoTHMACFunction,
-                                               const uint8_t * pucKey,
-                                               uint32_t ulKeySize,
-                                               const uint8_t * pucMessage,
-                                               uint32_t ulMessageSize,
-                                               uint8_t * pucBuffer,
-                                               uint32_t ulBufferLength,
-                                               uint8_t * pucOutput,
-                                               uint32_t ulOutputSize,
-                                               uint32_t * pulOutputLength );
-
 #include <azure/core/_az_cfg_suffix.h>
 
 #endif /* AZURE_IOT_H */
