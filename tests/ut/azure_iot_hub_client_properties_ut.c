84eb2e04ee8393e0aa5347168c7e127dcf0d50de
Dane Walton <dane.walton@microsoft.com>
2021-08-23 13:59:12 -0700

    pr comments

--- tests/ut/azure_iot_hub_client_properties_ut.c
+++ tests/ut/azure_iot_hub_client_properties_ut.c
@@ -48,7 +48,9 @@ static const uint8_t ucTestJSONResponse[] =
  * {
  *   "one_component": {
  *     "thing_one": 1,
- *     "thing_two": "string"
+ *     "thing_two": "string",
+ *     "thing_three": true,
+ *     "thing_four": 40.2
  *   },
  *   "two_component": {
  *     "prop_one": 45,
@@ -60,7 +62,7 @@ static const uint8_t ucTestJSONResponse[] =
  *
  */
 static const uint8_t ucTestJSONVersion[] =
-    "{\"one_component\":{\"thing_one\":1,\"thing_two\":\"string\"},\"two_component\":{\"prop_one\":" \
+    "{\"one_component\":{\"thing_one\":1,\"thing_two\":\"string\",\"thing_three\":true,\"thing_four\":40.2},\"two_component\":{\"prop_one\":" \
     "45,\"prop_two\":\"foo\"},\"not_component\":42,\"$version\":5}";
 
 /*
@@ -438,6 +440,8 @@ static void testAzureIoTHubClientProperties_GetNextComponentProperty_Success( vo
     AzureIoTHubClient_t xTestIoTHubClient;
     AzureIoTJSONReader_t xJSONReader;
     AzureIoTJSONTokenType_t xTokenType;
+    bool xTokenBool;
+    double xTokenDouble;
     AzureIoTHubMessageType_t xResponseType = eAzureIoTHubPropertiesWritablePropertyMessage;
     AzureIoTHubClientPropertyType_t xPropertyType = eAzureIoTHubClientPropertyWritable;
     uint32_t ulVersion;
@@ -498,6 +502,8 @@ static void testAzureIoTHubClientProperties_GetNextComponentProperty_Success( vo
         }
         else if( strncmp( "one_component", ucComponentName, strlen( "one_component" ) ) == 0 )
         {
+            assert_int_equal( strlen( "one_component" ), ulComponentNameLength );
+
             if( AzureIoTJSONReader_TokenIsTextEqual( &xJSONReader, "thing_one", strlen( "thing_one" ) ) )
             {
                 /*Advance to property value */
@@ -517,6 +523,25 @@ static void testAzureIoTHubClientProperties_GetNextComponentProperty_Success( vo
                 assert_true( AzureIoTJSONReader_TokenIsTextEqual( &xJSONReader, "string", strlen( "string" ) ) );
                 assert_int_equal( AzureIoTJSONReader_NextToken( &xJSONReader ), eAzureIoTSuccess );
             }
+            else if( AzureIoTJSONReader_TokenIsTextEqual( &xJSONReader, "thing_three", strlen( "thing_three" ) ) )
+            {
+                /*Advance to property value */
+                assert_int_equal( AzureIoTJSONReader_NextToken( &xJSONReader ), eAzureIoTSuccess );
+                assert_int_equal( AzureIoTJSONReader_TokenType( &xJSONReader, &xTokenType ), eAzureIoTSuccess );
+                assert_int_equal( xTokenType, eAzureIoTJSONTokenTRUE );
+                assert_int_equal( AzureIoTJSONReader_NextToken( &xJSONReader ), eAzureIoTSuccess );
+            }
+            else if( AzureIoTJSONReader_TokenIsTextEqual( &xJSONReader, "thing_four", strlen( "thing_four" ) ) )
+            {
+                /*Advance to property value */
+                assert_int_equal( AzureIoTJSONReader_NextToken( &xJSONReader ), eAzureIoTSuccess );
+                assert_int_equal( AzureIoTJSONReader_TokenType( &xJSONReader, &xTokenType ), eAzureIoTSuccess );
+                assert_int_equal( xTokenType, eAzureIoTJSONTokenNUMBER );
+                assert_int_equal( AzureIoTJSONReader_GetTokenDouble( &xJSONReader, &xTokenDouble ), eAzureIoTSuccess );
+                assert_in_range( xTokenDouble, 40.19, 40.21 );
+                assert_true( xTokenDouble );
+                assert_int_equal( AzureIoTJSONReader_NextToken( &xJSONReader ), eAzureIoTSuccess );
+            }
             else
             {
                 /* A property was encountered that we don't recognize. Should not reach here. */
@@ -525,6 +550,8 @@ static void testAzureIoTHubClientProperties_GetNextComponentProperty_Success( vo
         }
         else if( strncmp( "two_component", ucComponentName, strlen( "two_component" ) ) == 0 )
         {
+            assert_int_equal( strlen( "two_component" ), ulComponentNameLength );
+
             if( AzureIoTJSONReader_TokenIsTextEqual( &xJSONReader, "prop_one", strlen( "prop_one" ) ) )
             {
                 /*Advance to property value */
