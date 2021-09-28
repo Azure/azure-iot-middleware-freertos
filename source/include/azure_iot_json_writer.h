e59fc9c993d3f6d5922adfb3e21e9941b4f22497
Dane Walton <dane.walton@microsoft.com>
2021-09-28 13:27:37 -0700

    update some header files and Doxyfile

--- source/include/azure_iot_json_writer.h
+++ source/include/azure_iot_json_writer.h
@@ -25,6 +25,10 @@
 #include "azure/core/az_json.h"
 #include "azure/core/_az_cfg_prefix.h"
 
+/**
+ * @struct AzureIoTJSONWriter_t
+ * @brief The struct to use for Azure IoT JSON writer functionality.
+ */
 typedef struct AzureIoTJSONWriter
 {
     struct
