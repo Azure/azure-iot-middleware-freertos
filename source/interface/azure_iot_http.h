01c36f7454a7a48684d7564323812f66d77189b0
Dane Walton <dawalton@microsoft.com>
2022-08-26 14:35:41 -0500

    update http code to pass in buffers

--- source/interface/azure_iot_http.h
+++ source/interface/azure_iot_http.h
@@ -17,10 +17,7 @@
 #include "azure_iot_http_port.h"
 #include "azure_iot_transport_interface.h"
 
-#define azureiothttpHEADER_BUFFER_SIZE            512
-#define azureiothttpCHUNK_DOWNLOAD_SIZE           4096
-#define azureiothttpCHUNK_DOWNLOAD_BUFFER_SIZE    ( azureiothttpCHUNK_DOWNLOAD_SIZE + 1024 )
-#define azureiothttpHttpRangeRequestEndOfFile     -1
+#define azureiothttpHttpRangeRequestEndOfFile    -1
 
 typedef AzureIoTHTTP_t * AzureIoTHTTPHandle_t;
 
@@ -50,22 +47,30 @@ AzureIoTHTTPResult_t AzureIoTHTTP_Init( AzureIoTHTTPHandle_t xHTTPHandle,
                                         const char * pucURL,
                                         uint32_t ulURLLength,
                                         const char * pucPath,
-                                        uint32_t ulPathLength );
+                                        uint32_t ulPathLength,
+                                        char * pucHeaderBuffer,
+                                        uint32_t ulHeaderBufferLength );
 
 AzureIoTHTTPResult_t AzureIoTHTTP_Request( AzureIoTHTTPHandle_t xHTTPHandle,
                                            int32_t lRangeStart,
                                            int32_t lRangeEnd,
-                                           char ** ppucDataBuffer,
-                                           uint32_t * pulDataLength );
+                                           char * pucDataBuffer,
+                                           uint32_t ulDataBufferLength,
+                                           char ** ppucOutData,
+                                           uint32_t * pulOutDataLength );
 
 AzureIoTHTTPResult_t AzureIoTHTTP_RequestSizeInit( AzureIoTHTTPHandle_t xHTTPHandle,
                                                    AzureIoTTransportInterface_t * pxHTTPTransport,
                                                    const char * pucURL,
                                                    uint32_t ulURLLength,
                                                    const char * pucPath,
-                                                   uint32_t ulPathLength );
+                                                   uint32_t ulPathLength,
+                                                   char * pucHeaderBuffer,
+                                                   uint32_t ulHeaderBufferLength );
 
-int32_t AzureIoTHTTP_RequestSize( AzureIoTHTTPHandle_t xHTTPHandle );
+int32_t AzureIoTHTTP_RequestSize( AzureIoTHTTPHandle_t xHTTPHandle,
+                                  char * pucDataBuffer,
+                                  uint32_t ulDataBufferLength );
 
 AzureIoTHTTPResult_t AzureIoTHTTP_Deinit( AzureIoTHTTPHandle_t xHTTPHandle );
 
