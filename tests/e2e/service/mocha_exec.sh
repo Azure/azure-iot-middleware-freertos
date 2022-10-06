49c154b37339d710c537b95535dd9cdd61d5294c
Dane Walton <dawalton@microsoft.com>
2022-10-06 14:47:27 -0500

    run all sequence

--- tests/e2e/service/mocha_exec.sh
+++ tests/e2e/service/mocha_exec.sh
@@ -47,4 +47,7 @@ echo -e "Running tests"
 IOTHUB_CONNECTION_STRING=$IOTHUB_CONNECTION_STRING DEVICE_TEST_EXE=$DEVICE_TEST_EXE \
 IOT_PROVISIONING_CONNECTION_STRING=${IOT_PROVISIONING_CONNECTION_STRING:-""} \
 IOT_PROVISIONING_SCOPE_ID=${IOT_PROVISIONING_SCOPE_ID:-""} \
-npm run $1 --loglevel verbose
+npm run iothub_e2e_test --loglevel verbose
+npm run iothub_e2e_test_using_dm --loglevel verbose
+npm run iot_provisioning_e2e_test --loglevel verbose
+npm run iot_provisioning_e2e_test_using_dm --loglevel verbose
