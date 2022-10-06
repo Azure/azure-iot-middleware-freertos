d58e20d2a726313c1c1deb40c6015557d253bb21
Dane Walton <dawalton@microsoft.com>
2022-10-06 15:11:04 -0500

    try using run all

--- tests/e2e/service/mocha_exec.sh
+++ tests/e2e/service/mocha_exec.sh
@@ -47,7 +47,4 @@ echo -e "Running tests"
 IOTHUB_CONNECTION_STRING=$IOTHUB_CONNECTION_STRING DEVICE_TEST_EXE=$DEVICE_TEST_EXE \
 IOT_PROVISIONING_CONNECTION_STRING=${IOT_PROVISIONING_CONNECTION_STRING:-""} \
 IOT_PROVISIONING_SCOPE_ID=${IOT_PROVISIONING_SCOPE_ID:-""} \
-npm run iothub_e2e_test --loglevel verbose
-npm run iothub_e2e_test_using_dm --loglevel verbose
-npm run iot_provisioning_e2e_test --loglevel verbose
-npm run iot_provisioning_e2e_test_using_dm --loglevel verbose
+npm run $1
