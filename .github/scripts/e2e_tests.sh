f46f83d07cfb50ed36273a42c685ceca0a3177ab
Dane Walton <dawalton@microsoft.com>
2022-10-24 17:41:19 -0700

    only run adu for now

--- .github/scripts/e2e_tests.sh
+++ .github/scripts/e2e_tests.sh
@@ -10,8 +10,8 @@ set -o pipefail # Exit if pipe failed.
 echo -e "Using FreeRTOS in libraries/FreeRTOS (`git name-rev --name-only HEAD`)"
 TEST_FREERTOS_SRC=`pwd`/libraries/FreeRTOS
 
-echo -e "Running IoT Hub and Device Provisioning E2E"
-./tests/e2e/iot/run.sh veth1 $TEST_FREERTOS_SRC
+# echo -e "Running IoT Hub and Device Provisioning E2E"
+# ./tests/e2e/iot/run.sh veth1 $TEST_FREERTOS_SRC
 
 echo -e "Running ADU E2E"
 ./tests/e2e/adu/run.sh veth1 $TEST_FREERTOS_SRC
