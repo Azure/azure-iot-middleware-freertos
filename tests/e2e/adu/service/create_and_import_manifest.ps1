0537e499640d233bf8b58bccfda626b9787ec92d
Dane Walton <dawalton@microsoft.com>
2022-10-26 11:37:03 -0700

    restore regular e2e

--- tests/e2e/adu/service/create_and_import_manifest.ps1
+++ tests/e2e/adu/service/create_and_import_manifest.ps1
@@ -71,13 +71,17 @@ Param(
     [string] $UpdateName
 )
 
-Install-Module MSAL.PS -Force
-Install-Module Az.Accounts -Force
-Install-Module Az.Storage -Force
-Import-Module ../e2e_build/iot-hub-device-update/tools/AduCmdlets/AduUpdate.psm1 -ErrorAction Stop -Force
-Import-Module ../e2e_build/iot-hub-device-update/tools/AduCmdlets/AduImportUpdate.psm1 -ErrorAction Stop -Force
-Import-Module ../e2e_build/iot-hub-device-update/tools/AduCmdlets/AduRestApi.psm1 -ErrorAction Stop -Force
+# Errors in system routines will stop script execution
+$errorActionPreference    = "stop"
 
+Install-Module MSAL.PS
+Install-Module Az.Accounts
+Install-Module Az.Storage
+Import-Module ../e2e_build/iot-hub-device-update/tools/AduCmdlets/AduUpdate.psm1 -ErrorAction Stop
+Import-Module ../e2e_build/iot-hub-device-update/tools/AduCmdlets/AduImportUpdate.psm1 -ErrorAction Stop
+Import-Module ../e2e_build/iot-hub-device-update/tools/AduCmdlets/AduRestApi.psm1 -ErrorAction Stop
+
+# Set up Azure Account using the passed AAD secret
 $accountSecret = ConvertTo-SecureString -String $AzureAdApplicationSecret -AsPlainText -Force
 $accountCredential = [System.Management.Automation.PSCredential]::new($AzureAdClientId, $accountSecret)
 Connect-AzAccount -ServicePrincipal -Credential $accountCredential -Tenant $AzureAdTenantId -ErrorAction Stop
@@ -97,8 +101,8 @@ $updateInput = New-AduImportUpdateInput -UpdateId $updateId `
                                          -ErrorAction Stop `
                                          -Verbose:$VerbosePreference
 
+# Get a MSAL token for blob upload
 $secureStringAuth = ConvertTo-SecureString $AzureAdApplicationSecret -AsPlainText -Force
-
 $AuthorizationToken = Get-MsalToken `
             -ClientId $AzureAdClientId `
             -TenantId $AzureAdTenantId `
@@ -108,31 +112,30 @@ $AuthorizationToken = Get-MsalToken `
             -ErrorAction Stop
 
 # Import update into blob storage for deployment
+Write-Host("Starting import")
 $operationId = Start-AduImportUpdate -AccountEndpoint $AccountEndpoint `
                                      -InstanceId $InstanceId `
                                      -AuthorizationToken $AuthorizationToken.AccessToken `
                                      -ImportUpdateInput $updateInput `
                                      -Verbose:$VerbosePreference
-
 Wait-AduUpdateOperation -AccountEndpoint $AccountEndpoint `
                         -InstanceId $InstanceId `
                         -AuthorizationToken $AuthorizationToken.AccessToken `
                         -OperationId $operationId `
-                        -Timeout (New-TimeSpan -Minutes 15) `
+                        -Timeout (New-TimeSpan -Minutes 5) `
                         -Verbose:$VerbosePreference
 
 
 # Deploy the update to the device group
-$CurrentTime = [DateTime]::UtcNow.ToString('u')
-$DeploymentId = [guid]::NewGuid().toString()
-
-$deploymentUri = "https://$AccountEndpoint/deviceUpdate/$InstanceId/management/groups/$GroupID/deployments/$DeploymentId/?api-version=2021-06-01-preview"
+$currentTime = [DateTime]::UtcNow.ToString('u')
+$deploymentId = [guid]::NewGuid().toString()
+$deploymentUri = "https://$AccountEndpoint/deviceUpdate/$InstanceId/management/groups/$GroupID/deployments/$deploymentId/?api-version=2021-06-01-preview"
 $headers = @{'Authorization' = "Bearer $($AuthorizationToken.AccessToken)"}
 $headers.Add('Content-Type', 'application/json')
 $payload = @"
 {
-  "deploymentId": "$DeploymentId",
-  "startDateTime": "$CurrentTime",
+  "deploymentId": "$deploymentId",
+  "startDateTime": "$currentTime",
   "updateId": {
     "provider": "$UpdateProvider",
     "name": "$UpdateName",
@@ -143,7 +146,6 @@ $payload = @"
 "@
 
 Write-Host($deploymentUri)
-Write-Host($headers)
 Write-Host($payload)
 
 $response = Invoke-WebRequest -Uri $deploymentUri -Method PUT -Headers $headers -Body $payload -UseBasicParsing -Verbose:$VerbosePreference
@@ -152,7 +154,6 @@ Write-Host($response)
 
 # Get the status of the deployment
 $deploymenetStatusUri = "https://$AccountEndpoint/deviceupdate/$InstanceId/management/groups/$groupId/deployments/$deploymentId/status?api-version=2021-06-01-preview"
-
 $statusResponse = Invoke-WebRequest -Uri $deploymenetStatusUri -Method GET -Headers $headers -UseBasicParsing -Verbose:$VerbosePreference
 
 Write-Host($statusResponse)
