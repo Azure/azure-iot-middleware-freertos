35dd34ea98b209e330365dd8c1f83b5908ebf51f
Dane Walton <dawalton@microsoft.com>
2022-11-17 17:46:04 -0500

    add docs on limitations of features | update gitignore (#291)

--- source/include/azure_iot_adu_client.h
+++ source/include/azure_iot_adu_client.h
@@ -3,6 +3,10 @@
  *
  * @brief Definition for the Azure IoT ADU Client.
  *
+ * @warning Below is a limitation of features compared to ADU service capabilities:
+ * - Proxy updates are not supported: https://learn.microsoft.com/azure/iot-hub-device-update/device-update-proxy-updates
+ * - Delta updates are not supported: https://learn.microsoft.com/azure/iot-hub-device-update/understand-device-update#flexible-features-for-updating-devices
+ *
  */
 #ifndef AZURE_IOT_ADU_CLIENT_H
 #define AZURE_IOT_ADU_CLIENT_H
