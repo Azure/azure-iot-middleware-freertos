c9c0e452e7ff1b2035e71dc00b1d6dda12203ccf
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2023-02-15 13:51:46 -0800

    Code style fixes

--- source/include/azure_iot_hub_client.h
+++ source/include/azure_iot_hub_client.h
@@ -409,7 +409,7 @@ AzureIoTResult_t AzureIoTHubClient_UnsubscribeCloudToDeviceMessage( AzureIoTHubC
 
 /**
  * @brief Subscribe to Azure IoT Hub Direct Methods.
- * @remark Azure IoT Direct methods may also be referred to as Commands. 
+ * @remark Azure IoT Direct methods may also be referred to as Commands.
  *
  * @param[in] pxAzureIoTHubClient The #AzureIoTHubClient_t * to use for this call.
  * @param[in] xCommandCallback The #AzureIoTHubClientCommandCallback_t to invoke when command messages arrive.
@@ -424,7 +424,7 @@ AzureIoTResult_t AzureIoTHubClient_SubscribeCommand( AzureIoTHubClient_t * pxAzu
 
 /**
  * @brief Unsubscribe from Azure IoT Hub Direct Methods.
- * @remark Azure IoT Direct methods may also be referred to as Commands. 
+ * @remark Azure IoT Direct methods may also be referred to as Commands.
  *
  * @param[in] pxAzureIoTHubClient The #AzureIoTHubClient_t * to use for this call.
  * @return An #AzureIoTResult_t with the result of the operation.
