93193c4d74ee0cda2cb86ed7ff3ce0d4dc123e6c
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2025-09-18 13:31:17 -0700

    Replace deprecated function az_iot_provisioning_client_get_request_payload

--- source/azure_iot_provisioning_client.c
+++ source/azure_iot_provisioning_client.c
@@ -310,7 +310,7 @@ static void prvProvClientRequest( AzureIoTProvisioningClient_t * pxAzureProvClie
 
         xMQTTPayloadLength = pxAzureProvClient->_internal.ulScratchBufferLength - ( uint32_t ) xMQTTTopicLength;
         xCoreResult =
-            az_iot_provisioning_client_get_request_payload( &pxAzureProvClient->_internal.xProvisioningClientCore,
+            az_iot_provisioning_client_register_get_request_payload( &pxAzureProvClient->_internal.xProvisioningClientCore,
                                                             xCustomPayloadProperty, NULL,
                                                             ( uint8_t * ) ( pxAzureProvClient->_internal.pucScratchBuffer +
                                                                             xMQTTTopicLength ),
