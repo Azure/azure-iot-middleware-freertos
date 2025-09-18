be0d7fb55906eb1beab4c791175409e7c1ab9bc3
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2025-09-18 14:03:24 -0700

    Fix undefined SdkLog for coreHTTP

--- .github/config/core_http_config.h
+++ .github/config/core_http_config.h
@@ -137,6 +137,10 @@ extern void vLoggingPrintf( const char * pcFormatString,
 #define SINGLE_PARENTHESIS_LOGD( x, ... )    vLoggingPrintf( x, ## __VA_ARGS__ )
 #define LogDebug( message )                  SINGLE_PARENTHESIS_LOGD message
 
+#ifndef SdkLog
+    #define SdkLog( x, ... )                 vLoggingPrintf( x, ## __VA_ARGS__ )
+#endif
+
 /* *INDENT-OFF* */
 #ifdef __cplusplus
     }
