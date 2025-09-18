f4d9f8f71eb0a9c4eda614987acb01ee2bf4ea64
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2025-09-18 14:27:15 -0700

    Fix comp arg type errors

--- .github/config/core_http_config.h
+++ .github/config/core_http_config.h
@@ -138,7 +138,7 @@ extern void vLoggingPrintf( const char * pcFormatString,
 #define LogDebug( message )                  SINGLE_PARENTHESIS_LOGD message
 
 #ifndef SdkLog
-    #define SdkLog( x, ... )                 vLoggingPrintf( x, ## __VA_ARGS__ )
+    #define SdkLog( x )                 vLoggingPrintf( x )
 #endif
 
 /* *INDENT-OFF* */
