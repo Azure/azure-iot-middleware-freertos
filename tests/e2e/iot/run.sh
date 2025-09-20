3efc7068e38badd17459eb0d94e26822998e45f3
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2025-09-19 21:25:10 -0700

    Disable showing test usage and map file

--- tests/e2e/iot/run.sh
+++ tests/e2e/iot/run.sh
@@ -48,10 +48,11 @@ sed -i "s/#define configNETWORK_INTERFACE_TO_USE.*/#define configNETWORK_INTERFA
 echo -e "::group::Building E2E tests"
 cd $dir/device; cmake -Bbuild -DCMAKE_BUILD_TYPE=MinSizeRel -DFREERTOS_DIRECTORY=$test_freertos_src_path ../ ; cmake --build build
 
-echo -e "::group::E2E tests usage of libaz_iot_middleware_freertos.a"
-grep -E  "0x\w{16}\s+0x\w+ .*libaz_iot_middleware_freertos.a" $dir/device/build/azure_iot_e2e_tests.map
-echo -e "::group::E2E tests Map file"
-cat $dir/device/build/azure_iot_e2e_tests.map
+echo "cmake return code: $?"
+#echo -e "::group::E2E tests usage of libaz_iot_middleware_freertos.a"
+#grep -E  "0x\w{16}\s+0x\w+ .*libaz_iot_middleware_freertos.a" $dir/device/build/azure_iot_e2e_tests.map
+#echo -e "::group::E2E tests Map file"
+#cat $dir/device/build/azure_iot_e2e_tests.map
 
 echo -e "::group::Running E2E tests"
 export DEVICE_TEST_EXE="$dir/device/build/azure_iot_e2e_tests"
