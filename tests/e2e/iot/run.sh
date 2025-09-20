4bbdcf44cbf9269eaee802eefb01c6792c9a1169
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2025-09-19 21:51:28 -0700

    Revert to use ubuntu-latest, disable e2e tests

--- tests/e2e/iot/run.sh
+++ tests/e2e/iot/run.sh
@@ -48,14 +48,14 @@ sed -i "s/#define configNETWORK_INTERFACE_TO_USE.*/#define configNETWORK_INTERFA
 echo -e "::group::Building E2E tests"
 cd $dir/device; cmake -Bbuild -DCMAKE_BUILD_TYPE=MinSizeRel -DFREERTOS_DIRECTORY=$test_freertos_src_path ../ ; cmake --build build
 
-echo "cmake return code: $?"
-echo -e "::group::E2E tests usage of libaz_iot_middleware_freertos.a"
-grep -E  "0x\w{16}\s+0x\w+ .*libaz_iot_middleware_freertos.a" $dir/device/build/azure_iot_e2e_tests.map
-echo -e "::group::E2E tests Map file"
-cat $dir/device/build/azure_iot_e2e_tests.map
+# echo "cmake return code: $?"
+# echo -e "::group::E2E tests usage of libaz_iot_middleware_freertos.a"
+# grep -E  "0x\w{16}\s+0x\w+ .*libaz_iot_middleware_freertos.a" $dir/device/build/azure_iot_e2e_tests.map
+# echo -e "::group::E2E tests Map file"
+# cat $dir/device/build/azure_iot_e2e_tests.map
 
-echo -e "::group::Running E2E tests"
-export DEVICE_TEST_EXE="$dir/device/build/azure_iot_e2e_tests"
-chown root $DEVICE_TEST_EXE
-chmod u+s $DEVICE_TEST_EXE
-cd $dir/service; stdbuf -o0 ./mocha_exec.sh
+# echo -e "::group::Running E2E tests"
+# export DEVICE_TEST_EXE="$dir/device/build/azure_iot_e2e_tests"
+# chown root $DEVICE_TEST_EXE
+# chmod u+s $DEVICE_TEST_EXE
+# cd $dir/service; stdbuf -o0 ./mocha_exec.sh
